11acb3f93788ed13d37a07cabdd460134c230460
Your Name <franmoranmato@gmail.com>
2023-10-11 00:11:41 +0200

    No me pilla los saltos de línea

--- get_next_line.c
+++ get_next_line.c
@@ -12,101 +12,88 @@
 
 #include "get_next_line.h"
 
-char	*limit_buf(char *buf)
+char	*real_buffer(char *buf)
 {
     int	    i;
     int	    j;
-    char    *ptr;
+    char    *str;
 
     i = 0;
     j = 0;
-    while (buf[i] != '\n')
+    str = NULL;
+    while (buf[i] && buf[i] != '\n')
 	i++;
-    ptr = (char *)ft_calloc(sizeof(char), (i + 2));
-    if (ptr == NULL)
-	return (NULL);
-    while (j < i)
+    if (buf[i] == 0)
+	return (buf);
+    else
     {
-	ptr[j] = buf[j];
-	j++;
+	str = (char *)ft_calloc(sizeof(char), (i+2));
+	if (!str)
+	    return (NULL);
+	while (j < i)
+	{
+	    str[j] = buf[j];
+	    j++;
+	}
+	return (str);
     }
-    return (ptr);
-}
-
-int buf_bool(char *buf)
-{
-    int i;
-
-    i = 0;
-    while(buf[i])
-    {
-	if (buf[i] == '\n')
-	    return (1);
-	i++;
-    }
-    return (0);
 }
 
 char	*get_next_line(int fd)
 {
-    static char	    *buf;
-    char	    *str;
-    size_t	    r_len;
-    char	    *temp;
-    char	    *n_buf;
+    static char	*buf;
+    char	*str;
+    char	*temp;
 
-    if (fd < 0)
+    if (fd < 0 || BUFFER_SIZE <= 0)
 	return (NULL);
     str = NULL;
     if (buf != NULL)
-	buf = ft_strchr(buf, '\n');
-    r_len = sizeof(buf) - 1;
-    while (read(fd, buf, r_len) > 0)
     {
-	buf[r_len] = 0;
-	if (buf_bool(buf) == 0)
+	buf = ft_strchr(buf, '\n'); //Usar temporal, puede que no haya un \n
+	if (buf != 0)
 	{
+	    temp = real_buffer(buf);
+	    if(!temp)
+		return (NULL);
+	    str = ft_strdup(temp);
 	    if (str == NULL)
 	    {
-		str = ft_strdup(buf);
-		if (str == NULL)
-		    return (NULL);
+		free(temp);
+		return (NULL);
 	    }
-	    else
-	    {
-		temp = ft_strjoin(str, buf);
-		if (temp == NULL)
-		{
-		    free(str);
-		    return (NULL);
-		}
-		free (str);
-		str = temp;
-	    }
+	}
+    }
+    else
+    {
+	buf = ft_calloc(1, 1);
+	if (!buf)
+	    return (NULL);
+    }
+    while (read(fd, buf, BUFFER_SIZE) > 0)
+    {
+	buf[BUFFER_SIZE] = 0;
+	temp = real_buffer(buf);
+	if (!temp)
+	{
+	    free(temp);
+	    return (NULL);
+	}
+	if (!str)
+	{
+	    str = ft_strdup(temp);
+	    if(!str)
+		return (NULL);
 	}
 	else
 	{
-	    if (str == NULL)
-	    {
-		str = limit_buf(buf);
-		if (str == NULL)
-		    return (NULL);
-		return (str);
-	    }
-	    else
-	    {
-		n_buf = limit_buf(buf);
-		temp = ft_strjoin(str, n_buf);
-		if (temp == NULL)
-		{
-		    free(str);
-		    return (NULL);
-		}
+	    str = ft_strjoin(str, temp);
+	    if (!str)
+	    {	
 		free(str);
-		str = temp;
-		return (str);
+		return (NULL);
 	    }
-	} 
+	}
     }
     return (str);
 }
@@ -120,9 +107,9 @@ int	main(void)
 	str = get_next_line(fd);
 	printf("%s\n", str);
 	free (str);
-	str = get_next_line(fd);
-	printf("%s\n", str);
-	free (str);
+//	str = get_next_line(fd);
+//	printf("%s\n", str);
+//	free (str);
 	close(fd);
 	return (0);
 }
